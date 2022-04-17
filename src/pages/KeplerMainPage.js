a407b099c2f72dcb9968dd4f29c50332f25f2581
황원태 <amlk31255@gmail.com>
2022-04-17 21:16:13 +0900

    0417

--- src/pages/KeplerMainPage.js
+++ src/pages/KeplerMainPage.js
@@ -2,14 +2,12 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import caver from "klaytn/caver";
 import keplerContract from "klaytn/KeplerContract";
-// import fetch from "node-fetch";
 
 import Nav from "components/Nav";
 import Layout from "../components/Layout";
 
 import "./KeplerMainPage.scss";
 
-// const pinata = "https://gateway.pinata.cloud/ipfs/";
 class KeplerMainPage extends Component {
   constructor(props) {
     super(props);
