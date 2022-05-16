9f5fb20d92151ab4a43a5464a2f3ff9abb444d34
황원태 <amlk31255@gmail.com>
2022-05-16 18:22:05 +0900

    private

--- src/App.js
+++ src/App.js
@@ -22,12 +22,12 @@ const App = () => {
           <Route path="/mining" element={<KeplerMiningPage />} />
           <Route path="/evol" element={<KeplerEvolPage />} />
           <Route path="/shop" element={<KeplerShopPage />} />
-          {/* <Route path="/governance" element={<ErrorPage />} />
-          <Route path="/governance/proposal" element={<ErrorPage />} /> */}
+          <Route path="/governance" element={<ErrorPage />} />
+          <Route path="/governance/proposal" element={<ErrorPage />} />
           {/* <Route path="/governance" element={<Proposal />} /> */}
-          <Route path="/governance" element={<KeplerGovernancePage />} />
+          {/* <Route path="/governance" element={<KeplerGovernancePage />} />
           <Route path="/governance/:id" element={<KeplerGovernancePage />} />
-          <Route path="/governance/proposal" element={<KeplerProposalPage />} />
+          <Route path="/governance/proposal" element={<KeplerProposalPage />} /> */}
         </Routes>
       </div>
     </Router>
